7bc3c57296ce5ec3a701e7f2ca49049f5538d8b1
Omid Jaberi <omid.jaberi@sw-motion.cn>
2025-01-02 13:02:03 +0330

    Loosing logic added

--- NodeJS/src/Snake.js
+++ NodeJS/src/Snake.js
@@ -4,26 +4,35 @@ class Snake {
     #dir
     #width
     #height
+    #running
     constructor(width, height) {
         this.#width = width;
         this.#height = height;
         this.#snake = [[height / 2, width / 2], [height / 2, width / 2]];
         this.#dir = [0, 1];
         this.#food = null;
+        this.#running = true;
         this.spawnFood();
     }
     update() {
+        if (!this.#running) return false;
         let newHead = [
             (this.getHeight() + this.#dir[0] + this.#snake[0][0]) % this.getHeight(),
             (this.getWidth() + this.#dir[1] + this.#snake[0][1]) % this.getWidth()
         ];
+        if (!this.onFood(newHead))
+            this.#snake.pop();
+        if (this.onSnake(newHead)) {
+            this.#running = false;
+            return false;
+        }
         this.#snake.unshift(newHead);
         if (this.onFood(newHead))
             this.spawnFood();
-        else
-            this.#snake.pop();
+        return true;
     }
     changeDir(dir) {
+        if (!this.#running) return;
         this.#dir = dir;
     }
     onSnake([x, y]) {
@@ -37,6 +46,7 @@ class Snake {
         return this.#food && (x === this.#food[0] && y === this.#food[1]);
     }
     spawnFood() {
+        if (!this.#running) return;
         console.log("MEME");
         let free = [];
         for (let x = 0; x < this.getHeight(); x++)
