fbc9c3eb1107595d1e265d463e2b67b272584504
Omid Jaberi <omid.jaberie@gmail.com>
2025-01-09 09:53:59 +0330

    game update added

--- C++/src/snake_game.cpp
+++ C++/src/snake_game.cpp
@@ -3,6 +3,8 @@
 SnakeGame::SnakeGame(int _width, int _height) : width(_width), height(_height)
 {
     this->snake.push_back(std::make_pair(height / 2, width / 2));
+    this->snake.push_back(std::make_pair(height / 2, width / 2));
+    this->dir = std::make_pair(0, 1);
 }
 
 bool SnakeGame::onSnake(std::pair<int, int> cell)
@@ -16,7 +18,13 @@ bool SnakeGame::onFood(std::pair<int, int> cell)
 }
 
 void SnakeGame::update()
-{}
+{
+    std::pair<unsigned int, unsigned int> new_head;
+    new_head.first = (this->height + this->snake.front().first + this->dir.first) % this->height;
+    new_head.second = (this->width + this->snake.front().second + this->dir.second) % this->width;
+    this->snake.push_front(new_head);
+    this->snake.pop_back();
+}
 
 void SnakeGame::changeDir(std::pair<int, int> dir)
 {
