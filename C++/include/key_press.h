e6dead89bd1484f43b22947eaf97b6edd4505be5
Omid Jaberi <omid.jaberie@gmail.com>
2025-01-11 22:33:09 +0330

    key_press seperated

---
+++ C++/include/key_press.h
@@ -0,0 +1,11 @@
+#ifndef KEY_PRESS_H
+#define KEY_PRESS_H
+
+class KeyPress {
+public:
+    KeyPress();
+    char getKey();
+    ~KeyPress();
+};
+
+#endif
---
+++ C++/src/key_press.cpp
@@ -0,0 +1,47 @@
+#include "key_press.h"
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <ncurses.h>
+#endif
+
+KeyPress::KeyPress()
+{
+#ifdef _WIN32
+    // No setup required for Windows
+#else
+    initscr();
+    cbreak();
+    noecho();
+    nodelay(stdscr, TRUE); // Non-blocking input
+    keypad(stdscr, TRUE);
+#endif
+}
+
+KeyPress::~KeyPress()
+{
+#ifdef _WIN32
+    // No cleanup required for Windows
+#else
+    def_prog_mode(); // Retain the screen without clearing
+    //endwin();
+#endif
+}
+
+char KeyPress::getKey()
+{
+#ifdef _WIN32
+    if (_kbhit())
+	{
+        return _getch();
+    }
+    return 0;
+#else
+    int ch = getch();
+    if (ch != ERR)
+	{
+        return ch;
+    }
+    return 0;
+#endif
+}
