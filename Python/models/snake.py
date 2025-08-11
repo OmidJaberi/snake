07e751316c09ac21134acab24f3a8b73cd244a56
Omid Jaberi <omid.jaberie@gmail.com>
2025-08-11 12:03:31 +0330

    is_running() method added

--- Python/models/snake.py
+++ Python/models/snake.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import choice
 
 class SnakeGame:
     def __init__(self, width = 16, height = 16):
@@ -7,10 +7,9 @@ class SnakeGame:
         self.reset()
 
     def reset(self):
-        self.__dir = (0, 1)
-        self.__prev_dir = self.__dir
+        self.__prev_dir = self.__dir = (0, 1)
         self.__snake = [(int(self.__width / 2), int(self.__height / 2))]
-        self.__mouse = (int(self.__width / 2), int(self.__height / 2) + 1)
+        self.__mouse = (int(self.__width / 2) + self.__dir[0], int(self.__height / 2) + self.__dir[1])
 
     def get_width(self):
         return self.__width
@@ -25,23 +24,24 @@ class SnakeGame:
         return x == self.__mouse[0] and y == self.__mouse[1]
 
     def spawn(self):
-        while self.on_snake(self.__mouse[0], self.__mouse[1]):
-            self.__mouse = (randint(0, self.__width - 1), randint(0, self.__height - 1))
+        self.__mouse = choice([(x, y) for x in range(self.__width) for y in range(self.__height) if not self.on_snake(x, y)])
 
     def change_dir(self, x, y):
         if self.__prev_dir == (-x, -y): return
         self.__dir = (x, y)
 
+    def is_running(self):
+        return self.__snake[-1] not in self.__snake[:-1]
+
     def update(self):
-        head = self.__snake[-1]
-        new_head = (
-            (self.__width + head[0] + self.__dir[0]) % self.__width,
-            (self.__height + head[1] + self.__dir[1]) % self.__height
-        )
+        if not self.is_running(): return False
+        x, y = self.__snake[-1]
+        dx, dy = self.__dir
+        new_head = ((x + dx) % self.__width, (y + dy) % self.__height)
         self.__snake.append(new_head)
         if new_head == self.__mouse:
             self.spawn()
         else:
             self.__snake.pop(0)
         self.__prev_dir = self.__dir
-        return not new_head in self.__snake[:-1]
+        return self.is_running()
