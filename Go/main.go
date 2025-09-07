465a0a9e165835a2d22b61dbecf85d56753ea19e
Omid Jaberi <omid.jaberie@gmail.com>
2025-09-07 19:59:56 +0330

    Basic snake logic without loosing

--- Go/main.go
+++ Go/main.go
@@ -6,15 +6,17 @@ import (
 	"time"
 )
 
+var game *Game
+
 func keyPressHandler(key byte) {
-	if key == 'a' && x > 0 {
-		x--
-	} else if key == 'd' && x < 6 {
-		x++
-	} else if key == 'w' && y > 0 {
-		y--
-	} else if key == 's' && y < 6 {
-		y++
+	if key == 'a' {
+		game.changeDir(-1, 0)
+	} else if key == 'd' {
+		game.changeDir(1, 0)
+	} else if key == 'w' {
+		game.changeDir(0, -1)
+	} else if key == 's' {
+		game.changeDir(0, 1)
 	} else if key == 27 || key == 'q' { // Exit on ESC or Q
 		fmt.Println("Exiting...")
 		os.Exit(0)
@@ -22,6 +24,7 @@ func keyPressHandler(key byte) {
 }
 
 func main() {
+	game = newGame(12, 8)
 	// Set Terminal settings
 	oldState, err := makeRaw(int(os.Stdin.Fd()))
 	if err != nil {
@@ -34,7 +37,8 @@ func main() {
 
 	// Main thread
 	for {
-		draw()
+		draw(game)
+		game.update()
 		time.Sleep(200 * time.Millisecond)
 	}
 }
--- Go/ui.go
+++ Go/ui.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 )
 
-func draw() {
+func draw(game *Game) {
 	fmt.Print("\033[H\033[2J") // Clear Screen
-	for i := 0; i < 7; i++ {
-		for j := 0; j < 7; j++ {
-			if i == y && j == x {
+	for i := 0; i < game.height; i++ {
+		for j := 0; j < game.width; j++ {
+			if game.onSnake(j, i) {
 				fmt.Print("o ")
+			} else if game.onFood(j, i) {
+				fmt.Print("# ")
 			} else {
 				fmt.Print(". ")
 			}
