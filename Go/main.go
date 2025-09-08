cabc80c5443cb8d2bfb649cd611172350eb5bcec
Omid Jaberi <omid.jaberie@gmail.com>
2025-09-08 09:03:56 +0330

    Clean-Up

--- Go/main.go
+++ Go/main.go
@@ -6,39 +6,39 @@ import (
 	"time"
 )
 
-var game *Game
+const (
+	Delay = 200
+	Width = 12
+	Height = 8
+)
+
+var game *Game = newGame(Width, Height)
 
 func keyPressHandler(key byte) {
-	if key == 'a' {
+	switch key {
+	case 'a':
 		game.changeDir(-1, 0)
-	} else if key == 'd' {
+	case 'd':
 		game.changeDir(1, 0)
-	} else if key == 'w' {
+	case 'w':
 		game.changeDir(0, -1)
-	} else if key == 's' {
+	case 's':
 		game.changeDir(0, 1)
-	} else if key == 27 || key == 'q' { // Exit on ESC or Q
+	case 'q', 27:
 		fmt.Println("Exiting...")
 		os.Exit(0)
 	}
 }
 
 func main() {
-	game = newGame(12, 8)
-	// Set Terminal settings
 	oldState, err := makeRaw(int(os.Stdin.Fd()))
-	if err != nil {
-		panic(err)
-	}
+	if err != nil { panic(err) }
 	defer restore(int(os.Stdin.Fd()), oldState)
 
-	// Goroutine for keypress
 	go listenForKeyPress(keyPressHandler)
-
-	// Main thread
 	for {
-		draw(game)
 		game.update()
-		time.Sleep(200 * time.Millisecond)
+		draw(game)
+		time.Sleep(Delay * time.Millisecond)
 	}
 }
